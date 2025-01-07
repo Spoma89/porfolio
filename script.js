bc26d599260ed0a27a355bb559c06096fcda8989
Spoma89 <sebastianromeropoma89@gmail.com>
2025-01-07 11:35:39 -0300

    actuaizacon1

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function openCertificate(imageUrl) {
     window.open(imageUrl, '_blank');
 }
 
+// Obtener los elementos
+const menuToggle = document.getElementById('menuToggle');
+const menu = document.getElementById('menu');
+
+// Cambiar el estado del menú cuando se haga clic en el icono
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+});
